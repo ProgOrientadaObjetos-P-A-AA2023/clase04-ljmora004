7f5d5aa92b74bcc142aad5abbe202547b9635502
Luis <ljmora1@utpl.edu.ec>
2023-05-03 12:48:21 -0500

    Clase04

--- ejemplos-clases-constuctores-java/EjemploConstructores/src/paquete4/Ejecutor.java
+++ ejemplos-clases-constuctores-java/EjemploConstructores/src/paquete4/Ejecutor.java
@@ -9,7 +9,9 @@ public class Ejecutor {
     public static void main(String[] args) {
         // Se crea un objeto
         // haciendo referencia al constructor por defecto
-        FacturaTelefonica fac = new FacturaTelefonica();
+        FacturaTelefonica fac = new FacturaTelefonica("0334645", 56.33, 0.3);
+        fac.calcularValorFactura();
+        
         System.out.printf("Número telefónico: %s\n"
                 + "Minutos mes: %.2f\nValor minuto: %.2f\n"
                 + "Valor factura: %.2f\n", fac.obtenerNumeroTelefono(),
